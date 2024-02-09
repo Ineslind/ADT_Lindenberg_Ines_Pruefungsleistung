30135a45bd3383579cd38d23337ad0dd0ea0e7b4
ineslind <ines.lindenberg@googlemail.com>
2024-02-09 19:11:39 +0100

    Szenenumschaltung implementiert

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 setTimeout(() => {
     
-    document.querySelector(".Anweisung").classList.remove("versteckt");
+    document.querySelector(".Anweisung").classList.remove("versteckt"); 
+    document.getElementById("Start").addEventListener("click", event => {
+        document.getElementById("IntroSzene").classList.add("ausgeblendet")
+        document.getElementById("AnleitungSzene").classList.remove("ausgeblendet")
+        
+    })
 
 }, 500)
